b004fb93082b404ae334aa7c01b505384404f8a3
Alexander Mishin <alexander.mishin.2001@bk.ru>
2020-11-18 22:08:35 +0300

    чистый yii

--- demo/config/test.php
+++ demo/config/test.php
@@ -28,7 +28,7 @@ return [
             'identityClass' => 'app\models\User',
         ],
         'request' => [
-            'cookieValidationKey' => 'test',
+            'cookieValidationKey' => 'qwerty123',
             'enableCsrfValidation' => false,
             // but if you absolutely need it set cookie domain to localhost
             /*
