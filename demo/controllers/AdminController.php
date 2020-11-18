95e22782966c1ac1df840288db8d984373cf38a6
Alexander Mishin <alexander.mishin.2001@bk.ru>
2020-11-19 01:19:45 +0300

    Блокировка страницы админа

--- demo/controllers/AdminController.php
+++ demo/controllers/AdminController.php
@@ -12,13 +12,24 @@ use app\models\ContactForm;
 
 class AdminController extends Controller
 {
-   
+    public function beforeAction($action){
+        
+        if(Yii::$app->user->isGuest ){
+            return $this->redirect(['site/login']);
+         }
+        if( Yii::$app->user->identity->admin == 0){
+             return $this->redirect(['site/404.php']);
+          }
+        if(!parent::beforeAction($action)){
+            return false;
+        }
+        return true;
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
     }
 
-    
    
-    
 }
